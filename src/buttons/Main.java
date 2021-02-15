d2d124ca4be5b6d9ab207ce7f638122c03a70b84
chouguting <chouguting@gmail.com>
2021-02-15 15:30:00 +0800

    add new stuff

---
+++ src/buttons/Main.java
@@ -0,0 +1,11 @@
+package buttons;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        //JButton: 一個可編成的GUI按鈕
+
+        MyFrame frame=new MyFrame(); //利用Class創造Frame
+    }
+}
